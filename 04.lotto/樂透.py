766fbc57bd0870828caaa942fd6daf80c2c091dd
joy <ztex080158486@gmail.com>
2022-08-18 12:11:47 +0800

    lotto

--- 04.lotto/樂透.py
+++ 04.lotto/樂透.py
@@ -1,15 +1,15 @@
 #樂透號碼生成 隨機抽出六個不重複號碼
 import random
-numlotto =[]
+lotto =[]
 num49 = [i for i in range(1,50)]
 # print(num48)
 for i in range(6):
 #n1 = random.randint(1,48)
     n1 = random.choice(num49)
-    numlotto.append(n1)
-    numlotto.sort()
+    lotto.append(n1)
+    lotto.sort()
     num49.remove(n1)
     sp = random.choice(num49)
 
-print("中獎號碼",numlotto)
+print("中獎號碼",lotto)
 print("特別號:",sp)
